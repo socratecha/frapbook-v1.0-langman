9f0ad73611d718bafa1233f99173a1cfc54e5ed0
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:42:17 -0500

    Langman ORM v2

--- server/langman_orm.py
+++ server/langman_orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, types, MetaData
+from sqlalchemy import create_engine, Column, types, MetaData, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 meta = MetaData()
 base_games = declarative_base(meta)
@@ -17,4 +17,25 @@ class Usage(base_games):
     secret_word = Column(types.String(length=25), nullable=False)
     usage       = Column(types.String(length=500), nullable=False)
     source      = Column(types.String(length=100))
+
+class User(base_games):
+    '''Table ``users`` with fields:
+      * ``user_id`` - UUID primary key of length 38
+      * ``user_name`` - String of maximum length 30
+      * ``num_games`` - Integer count of games started
+      * ``outcomes`` - JSON string storing game counts by outcome
+      * ``by_lang`` - JSON string storing game counts by game language
+      * ``first_time`` - DateTime indicating when first game was played
+      * ``total_time`` - TimeDelta of total time with active games
+      * ``avg_time`` - TimeDelta of the average game length
+    '''
+    __tablename__ = 'users'
+    user_id        = Column(types.String(length=38), primary_key=True)
+    user_name      = Column(types.String(length=30), nullable=False)
+    num_games      = Column(types.Integer, default=0)
+    outcomes       = Column(types.Text, default='{}')
+    by_lang        = Column(types.Text, default='{}')
+    first_time     = Column(types.DateTime)
+    total_time     = Column(types.Interval)
+    avg_time       = Column(types.Interval)
 
