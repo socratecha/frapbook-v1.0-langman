47589da2e5d3911820b3500a5a6d825421f48e05
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:42:38 -0500

    Langman ORM v4

--- server/prepare_orm.py
+++ server/prepare_orm.py
@@ -1,8 +1,9 @@
 import os
+import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from .langman_orm import base_games
+from .langman_orm import base_games, Usage
 from .util import get_config
 
 import click
@@ -16,3 +17,21 @@ def init_db():
     db   = create_engine(config['DB'])
     base_games.metadata.create_all(db)
                 
+    Session = sessionmaker(db)
+    session = Session()
+    if session.query(Usage).count() == 0:
+        data = []
+        R = csv.reader(open('data/usages.csv', encoding='utf8'))
+        for row in R:
+            if len(row[4]) <= 500:
+                data.append( Usage(
+                    usage_id    = int(row[0]),
+                    language    = row[1],
+                    secret_word = row[3],
+                    usage       = row[4],
+                    source      = row[5]
+                ))
+
+        print('Adding', len(data), 'rows to Usage table')
+        session.add_all(data)
+        session.commit()
